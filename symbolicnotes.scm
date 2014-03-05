83256f66e0a0e266d1b696231fcf1047f3927db9
Futrell <futrell@mit.edu>
2014-03-05 12:08:40 -0500

    notes 3/5/14

--- symbolicnotes.scm
+++ symbolicnotes.scm
@@ -693,7 +693,535 @@
 (fact 0) = 1
 (fact n) = (* n (fact (- n 1))) ; need to add a guard enforcing n > 0
 
+;;; 2-26-2014
+;;;
+;;; The nice things about rules is that they're somewhat
+;;; additive. Next time we'll talk about why this approach is
+;;; inadequate.
+
+(define algebra-1
+  (rule-simplifier
+   (list
+    ;; Associative law
+    (rule '(+ (? a) (+ (? b) (? c)))
+	  `(+ (+ ,a ,b) ,c))
+
+    ;; Commutative law
+    (rule '(* (? b) (? a))
+	  (and (expr<? a b) ; an arbitrary total ordering
+	       `(* ,a ,b)))
+
+    ;; Distributive law
+    (rule '(* (? a) (+ (? b) (? c)))
+	  `(+ (* ,a ,b) (* ,a ,c))))))
+
+;;; Now you can make a bigger system! 
+
+(define algebra-2
+  (rule-simplifier
+   (list
+    (rule `(+ (? a)) a)
+    (rule `(+ (?? a) (? y) (? x) (?? b))
+	  (and (expr<? x y)
+	       `(+ ,@a ,x ,y ,@b)))
+    (rule `(* (? a)) a)
+    (rule `(* (?? a) (* (?? b)) (?? c))
+	  `(* ,@a ,@b ,@c))
+    ;; etc. 
+
+    (rule `(+ 0 (?? x)) `(+ ,@x))
+    (rule `(+ (? x ,number?) (? x ,number?) (?? z))
+	  `(+ ,(+ x y) ,@z))
+
+    (rule `(* 0 (?? x)) 0)
+    (rule `(* 1 (?? x)) `(* ,@x))
+    )))
+
+;;; Use the total ordering to make numerical expressions bubble to the
+;;; left together so they can be simplified. 
+
+(define derivates
+  (rule-simplifier
+   (list (rule `(d/d (? u) (? x))
+	       (and (not (recursive-member? x u))
+		    0))
+	 (rule `(d/d (? x) (? x))
+	       1)
+	 (rule `(d/d (+ (? u) (? v)) (? x))
+	       `(+ (d/d ,u ,x)
+		   (d/d ,v ,x)))
+	 (rule `(d/d (* (? u) (? v)) (? x))
+	       `(+ (* (d/d ,u ,x) ,v)
+		   (* ,u (d/d ,v ,x))))
+	 (rule `(d/d (expt (? u) (? n ,integer?))
+		     (? x))
+	       `(* ,n
+		   (expt ,u ,(- n 1))
+		   (d/d ,u ,x))))))
+
+(define (recursive-member? x u)
+  (cond ((equal? x u) #t)
+	((pair? u)
+	 (or (recursive-member? x (car u))
+	     (recursive-member? x (cdr u))))
+	(else #f)))
+
+;;; Now how do I make this work?
+
+(define (rule-simplifier the-rules)
+  (define (simplify-expression expression)
+    (let ((subexpressions-simplified
+	   (if (list? expression)
+	       (map simplify-expression expression)
+	       (expression)))
+	  ;; ones the subexps are simplified we resimplify the
+	  ;; expression that contains them
+	  (try-rules subexpressions-simplified
+		     the-rules
+		     ;; if any rule applies we must resimplify
+		     (lambda (result fail) ;; oh I've got a new
+		       ;; result, now I have to go around the loop
+		       ;; simplifying that too. Also, what if it's the
+		       ;; case that the recursive simplifying
+		       ;; expression here fails? Need a more powerful
+		       ;; function.
+		       (simplify-expression result))
+		     ;; if no rules applies we're done
+		     (lambda () ; if no rule applied, just return that
+		       subexpression-simplified))))
+    (rule-memoize simplify-expression))
+		     
+(define (try-rules data rules succeed fail)
+  (let per-rule ((rules rules)) ; Q. why overwrite the name here?
+				; A. whatever
+    (if (null? rules)
+	(fail) ; if no more rules, call the failure continuation
+	((car rules) data succeed (lambda ()
+				    (per-rule (cdr rules)))))))
+
+;;; Macros are one of the best ways to confuse yourself.
+;;; In many languages macros are just substitution, but variable
+;;; capture is a problem. Hygienic macros are like procedures in that
+;;; they have local context, to prevent variable capture. A much more
+;;; powerful macro system is syntactic closures. 
+
+#| 
+(syntax '(rule ... ...))
+(make-rule ...
+	   (lambda (b a)
+	     ...))
+|#
+
+(define-syntax rule
+  (sc-macro-transformer
+   (lambda (form use-env)
+     (let ((patern (cadr form))
+	   (handler-body (caddr form))) ; stuff to prevent capture
+       ...))))
+
+;;; Get out of macro world as fast as possible!
+(define (make-rule pattern consequent)
+  (let ((match-procedure
+	 (match:->combinators pattern))
+	(bound-variables
+	 (procedure-bound-variables consequent)))
+    (define (the-rule data succeed fail)
+      (or (match-procedure (list data) '() ; if it succeeds I get a
+					; new dictionary where I have
+					; to look up the value for
+					; every bound variable in the
+					; dictionary; make that into
+					; an argument which I apply to
+					; the consequent to get the
+					; result ...
+)))))
+
+;;; The only reason we use macros is to produce syntactic sugar. 
+
+;;; 2-28-14
+;;;
+;;; Symbolic integration, which is not useful but is good for showing
+;;; off and problem sets. But it elicits a deep technique. 
+
+;;; SIN symbolic integrator: Joel Moses.
+;;; Derivative Divides Heuristic:
+;;; I(x) = \int c \rho(u(x)) u'(x) dx
+;;; let y=u(x) so dy = u'(x)dx, then I(x) = cJ(u(x)) where 
+;;; J(y) = \int \rho(y)dy
+
+(define (integration-by-parts)
+  (rule `(int (? x) (product (? u) (? dv/dx)))
+	(and (occurs? x u)
+	     (occurs x dv/sx)
+	     (let ((v (internal-integrate
+		       `(int ,x ,dv/dx))))
+	       (and v
+		    (let* ((du/dx (derivative u/x)))
+		      ...))))))
+
+;;; This depends on semantic match: you integrate by parts if you can
+;;; see something as (int (? x) (product (?u) (? dv/dx)).
+
+;;; Suppose I want to match quadratics: I might have to imagine terms.
+(define match-quadratic
+  (rule `(sum (product
+	       (expt (? var ,non-numeric?) 2)
+	       (? a ,non-zero?))
+	      (sum (product (? var) (? b))
+		   (? c))) 
+	(and (not (occurs? var a)) ; what does occurs? mean?
+	     (not (occurs? var b))
+	     (not (occurs? var c))
+	     (list var a b c))))
+
+(match-quadratic '(+ (* 2 (expt x 2)) (* 3 x) 4))
+;Value: (x 2 3 4)
+
+(match-quadratic '(+ (expt x 2) (* 3 x) 4)) ; need to imagine the 1
+;Value: (x 1 3 4) 
+
+(match-quadratic '(+ (* 3 x) 4))
+;Value: #f
+
+(define perfect-square?
+  (rule `(sum (product
+	       (expt (? var ,non-numeric?) 2)
+	       (? a ,non-zero?))
+	      (sum (product (? var) (? b))
+		   (? c)))
+	(and (not (occurs? var a))
+	     (not (occurs? var b))
+	     (not (occurs? var c))
+	     (zero? ; so it's a quadratic with this constraint: the
+		    ; discriminant turns out to be 0. 
+	      (simplify
+	       `(- (expt ,b 2)
+		   (* 4 ,a ,c))))
+	     (pp (list a b c var))
+	     #f)))
+
+(perfect-square? `(* (+ x y) (- x y)))
+;Value: #f
+
+(perfect-square? `(* (+ x y) (+ x y)))
+(1 (* 2 y) (expt y 2) x)
+(1 (* 2 x) (expt x 2) y)
+;Value: #f
+
+;;; Next week we go to evaluator land. How do you make a problem that
+;;; fits your problem?
+
+;;; 3-3-2014
+;;;
+;;; Eval is the universal machine.
+
+(define eval
+  (lambda (expr env) ; env is a mapping of names to their values
+    (cond ((number? exp) exp)
+	  ((symbol? exp) (lookup exp env))
+	  ((eq? (car expr) 'quote) (cadr expr))
+	  ((eq? (car expr) 'cond) (eval-clauses (cdr expr) env))
+	  ((eq? (car expr) 'lambda) (list 'procedure 
+					  (cadr expr)
+					  (caddr exp)
+					  env)) ; lexical scoping
+					    ; happens here
+	  (else (apply (eval (car expr) env)
+		       (values (cdr expr) env))))))
+
+(define apply
+  (lambda (proc args)
+    (cond ((primitive? proc) 
+	   (papply proc args))
+	  ((eq? (car proc) 'procedure)
+	   (eval (caddr proc) (extend (cadddr procedure) ; old environment
+				      (cadr proc) ; args from lambda
+				      args)))
+	  (else (error "Unknown procedure applied.")))))
+
+(define values
+  (lambda (exprs env)
+    (cond ((null? exprs) 
+	   '())
+	  ((else (cons (eval 9car exprs) env)
+		 (values (cdr exprs) env))))))
+
+(define eval-clauses
+  (lambda (clauses env)
+    (cond ((eq? (caar clauses) 'else)
+	   (eval (cadar clauses) env))
+	  ((not (eq? (eval (caar clauses) env) #f)) 
+	   (eval (cadar clauses) env))
+	  (else 
+	   (eval-clauses (cdr clauses) env)))))
+
+(define extend
+  (lambda (base-env vars vals)
+    (cons (make-frame vars vals) base-env)))
+
+(define make-frame
+  (lambda (vars vals)
+    (cond ((null? vars)
+	   (cond ((null? vals)
+		  '()) ; base case
+		 (else (error "Too many arguments."))))
+	  ((null? vals)
+	   (error "Too few arguments."))
+	  (else
+	   (cons (cons (car vals) (car vals))
+		 (make-frame (cdr vars) (cdr vals)))))))
+
+(define lookup
+  (lambda (var env)
+    (cond ((null? env)
+	   (error "Unbound variable."))
+	  (else ((lambda (binding) ; this is a let form
+		   (cond (binding
+			  (cdr binding))
+			 (else (lookup var (cdr env)))))
+		 (assq var (car env)))))))
+
+(define assq
+  (lambda (var bindings)
+    (cond ((null? bindings)
+	   #f)
+	  ((eq? var (caar bindings))
+	   (car bindings))
+	  (else (assq var (cdr bindings))))))
+
+(define primitive?
+  (lambda (proc)
+    (memq proc (list car cdr pair? cons eq? memq cadr ...))))
+
+(define papply
+  (lambda (proc args)
+    (cond ((eq? proc car) ; not quote car!
+	   (car (car args)))
+	  ((eq? proc cdr)
+	   (cdr (car args)))
+	  ...)))
+
+;;; What does this mean? Really, what does a program mean ever? 
+
+(define fact
+  (lambda (n)
+    (cond ((= n 0) 1)
+	  (else (* n (fact (- n 1)))))))
+
+;;; This is an equation in the name fact which has the property that
+;;; one of its solutions is the method of computing factorials. 
+
+K = (lambda (fact)
+      (lambda (n)
+	(cond ((= n 0) 1)
+	      (else (* n (fact (- n 1)))))))
+
+;;; I'm asking for a fixpoint of K. The true factorial procedure ! is
+;;; a thing that has the property that ! = (K !). Because if you pass !
+;;; into K then it's the fact in lambda (fact). 
+
+;;; Say we have B, bottom, the useless procedure that always gives an
+;;; error. (K B) is a good factorial procedure for n=0. 
+;;; Now if I have (K (K B)), that'll give me the value for n=1.
+;;; (K (K (K B))) is good up to n=2.
+
+;;; I'll give you a procedure Y.
+Y = (lambda (f)
+      ((lambda (x) (f (x x)))
+       (lambda (x) (f (x x)))))
+
+(Y K) = (K (K (K (K ...)))) = (K (Y K)) = !
+
+;;; This one works only in normal order. The website has one for
+;;; applicative order.
 
 
 
+;;; 3-5-2014
 
+;;; Easy way to see what's going on with Y operators and such:
+((lambda (g) (g g 20))
+ (lambda (f n)
+   (cond ((< n 2) n)
+	 (else (+ f f (- n 2))
+	       (f f (- n 1))))))
+;Value: 6765
+
+(define sigma
+  (lambda (f lo hi)
+    (define lp
+      (lambda (i sum)
+	(if (> i hi)
+	    sum
+	    (lp (+ i 1) (+ sum (f i))))))
+    (lp lo 0)))
+
+(define sum-powers 
+  (lambda (lo hi n)
+    (define nth-power 
+      (lambda (x) (expt x n)))
+    (sigma nth-power lo hi)))
+
+;;; This demonstrates static lexical binding: the n has the same value
+;;; in sum-powers and in nth-power. Where does that occur in an
+;;; interpreter? And then I'll show you alternatives.
+
+;;; The critical part is the part of eval where we evaluate a lambda
+;;; expression to make a procedure. You capture the environment of the
+;;; lambda expression when you make do make:
+
+(list 'PROCEDURE (cadr exp) (caddr exp) env)
+
+;;; So the n in sum-powers is given its value when the function is
+;;; DEFINED, not when it's called. Then in apply, you have:
+
+(extend (cadddr proc) (cadr proc) args)
+
+;;; Now for some other ideas. How about this?
+
+(define nth-power
+  (lambda (x) (expt x n)))
+
+(define sum-powers
+  (lambda (lo hi n)
+    (sigma nth-power lo hi)))
+
+;;; In that case the n comes not from the place where nth-power is
+;;; defined, but rather from where it's called. Early LISPs did this
+;;; and APL too. "Dynamic binding". Is this more modular? Before
+;;; arguing, I'll show you how to do it, and how we can do both if we
+;;; want using "fluid variables".
+
+;;; New rules inside eval:
+(list 'PROCEDURE
+      (cadr exp)
+      (caddr exp))
+
+(apply (eval (car exp) env)
+       (values (cdr exp) env)
+       env) ; pass in the environment too
+
+;;; Then within apply you have:
+(eval (caddr proc)
+      (extend env
+	      (cadr proc)
+	      args))
+
+;;; OK so why is this a bad idea? Suppose the library routine were the
+;;; following:
+
+(define sigma
+  (lambda (f lo hi)
+    (define lp
+      (lambda (n sum)
+	(if (> n hi)
+	    sum
+	    (lp (+ n 1) (sum f n)))))
+    (lp lo 0)))
+
+;;; In that case the internal n would be captured. Aagh! 
+;;; Dynamic binding leads to referential opacity.
+;;; Things depend upon names that you can't know.
+
+;;; Now why is that ever useful? It's good when you want something to
+;;; be controlled by an extent of time, such as in an operating system
+;;; when you want to know when files are opened or close. Also,
+;;; exception handlers. 
+
+;;; Here's a simpler bit of code using static lexical binding, using
+;;; currying.
+
+(define nth-power
+  (lambda (n)
+    (lambda (x) expt x n)))
+
+(define sum-powers
+  (lambda (lo hi n)
+    (sigma (nth-power n) lo hi)))
+
+;;; (Argument about Haskell)
+
+;;; Let's go down a different path: continuations, which represent
+;;; everything that will happen in the future. 
+
+(+ (* 3 4) 5)
+
+;;; Rewrite that as:
+
+(** 3 4
+    (lambda (v)
+      (++ v 5 k))) ; k is the continuation
+
+;;; The value of this one goes to the k. Instead of returnign the
+;;; value directly it calls a continuation which gets a value, adds
+;;; it to 5, and then calls ITS Continuation function. We're escaping
+;;; from expression world. Once you get this you can do all sorts of
+;;; things like write an entire operating system which requires stuff
+;;; like interrupts. 
+
+;;; Let's define call/cc or call-with-current-continuation:
+
+(+ 1 (call/cc 
+      (lambda (k) 
+	(+ 2 (k 3)))))
+;Value: 4
+
+;;; At the top level is something that adds 1 to something. Call/cc
+;;; says capture that continuation and give it the name k. Call k on 3
+;;; later. The result is 4. (k 3) says take that 3 and put it in the
+;;; place of call/cc: make that the thing that the continuation
+;;; accepts. 
+
+((lambda (map)
+   (call/cc
+    (lambda (k)
+      (map map ; ha ha that's funny
+	   (lambda (x)
+	     (cond ((negative? x) (k 'bad))
+		   (else (sqrt x))))
+	   '(1 9 25 64)))))
+ (lambda (me proc lst)
+   (cond ((null? lst) lst)
+	 (else
+	  (cons (proc (car lst))
+		(me me proc (cdr lst))))))
+;Value: (1 3 5 8) 
+
+;;; But if I "pass in" '(1 9 -35 64) I get the output 'bad. 
+;;; The continuation is functioning like break. 
+;;; But a continuation may be called more than once.
+
+(define r #f)
+
+(+ 1 (call/cc
+      (lambda (k)
+	(set! r k)
+	(+ 2 (k 3)))))
+;Value: 4
+
+(r 5)
+;Value: 6
+
+(+ 3 (r 5))
+;Value: 6
+
+;;; The future of the computation has been captured so it can be used
+;;; more than once. Useful for generate-and-test. This gives you
+;;; dangerous power.
+
+(define eval
+  (lambda (exp env)
+    (eval-k exp env (lambda (x) x))))
+
+;;; Add this in eval-k:
+((eq? (car exp) 'CALL/CC)
+ (eval-k (cadr exp) env ; the argument of call/cc
+	 (lambda (proc) (apply-k proc (list k k)))))
+
+;;; I've changed the evaluator's shape which has consequences. How can
+;;; I prevent inventing other extensions to the evaluator when I add
+;;; more things?
+
+;;; It turns out continuations are universal. In Haskell there's a
+;;; paper called "The mother of all monads". It argues that
+;;; continuations give you everything you can do with a monad. 
